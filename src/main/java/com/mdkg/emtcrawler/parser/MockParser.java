f5d77c4c3db758018ada03f7a49bc2369ec30c98
DimovskiM <mihajlo.d96@hotmail.com>
2018-12-18 22:00:13 +0100

    Added parsing functionality.

--- src/main/java/com/mdkg/emtcrawler/parser/MockParser.java
+++ src/main/java/com/mdkg/emtcrawler/parser/MockParser.java
@@ -22,14 +22,14 @@ public class MockParser {
 
     Document document;
 
-    static String GRICKI_SOLENKI_SMOKI = "http://www.marketonline.mk/%D0%A7%D0%B8%D0%BF%D1%81%2C-%D1%81%D0%BC%D0%BE%D0%BA%D0%B8%2C-%D1%81%D0%BE%D0%BB%D0%B5%D0%BD%D0%BA%D0%B8%2C-%D0%B3%D1%80%D0%B8%D1%86%D0%BA-41/";
-    static String MLECHNI_PROIZVODI = "http://www.marketonline.mk/%D0%9C%D0%BB%D0%B5%D1%87%D0%BD%D0%B8-%D0%BF%D1%80%D0%BE%D0%B8%D0%B7%D0%B2%D0%BE%D0%B4%D0%B8-6/";
-    static String SIRENJE = "http://www.marketonline.mk/%D0%A1%D0%B8%D1%80%D0%B5%D1%9A%D0%B5-2/";
-    static String MLEKO_JOGURT_KISELO_MLEKO="http://www.marketonline.mk/%D0%9C%D0%BB%D0%B5%D0%BA%D0%BE%2C-%D1%98%D0%BE%D0%B3%D1%83%D1%80%D1%82%2C-%D0%BA%D0%B8%D1%81%D0%B5%D0%BB%D0%BE-%D0%BC%D0%BB%D0%B5%D0%BA%D0%BE-48/";
-    static String LEB = "http://www.marketonline.mk/%D0%9B%D0%B5%D0%B1-%D0%B8-%D0%B1%D0%B5%D0%BB%D0%B8-%D0%BF%D0%B5%D1%86%D0%B8%D0%B2%D0%B0-%D0%9B%D0%B5%D0%B1-19/";
-    static String PIVO = "http://www.marketonline.mk/%D0%90%D0%BB%D0%BA%D0%BE%D1%85%D0%BE%D0%BB%D0%BD%D0%B8-%D0%BF%D0%B8%D1%98%D0%B0%D0%BB%D0%BE%D1%86%D0%B8-%D0%9F%D0%B8%D0%B2%D0%BE-64/";
-    static String JAJCA ="http://www.marketonline.mk/%D0%88%D0%B0%D1%98%D1%86%D0%B0-%D0%88%D0%B0%D1%98%D1%86%D0%B0-277/";
-    static String SVEZH_ZELENCHUK = "http://www.marketonline.mk/%D0%97%D0%B5%D0%BB%D0%B5%D0%BD%D1%87%D1%83%D0%BA-%D0%9E%D1%81%D1%82%D0%B0%D0%BD%D0%B0%D1%82-%D0%B7%D0%B5%D0%BB%D0%B5%D0%BD%D1%87%D1%83%D0%BA-296/";
+    static String GRICKI_SOLENKI_SMOKI = "page0.html";
+    static String MLECHNI_PROIZVODI = "page1.html";
+    static String SIRENJE = "page2.html";
+    static String MLEKO_JOGURT_KISELO_MLEKO="page3.html";
+    static String LEB = "page4.html";
+    static String PIVO = "page5.html";
+    static String JAJCA ="page6.html";
+    static String SVEZH_ZELENCHUK = "page7.html";
     List<String> stringList;
 
     public MockParser() {
--- src/main/java/com/mdkg/emtcrawler/parser/Parser.java
+++ src/main/java/com/mdkg/emtcrawler/parser/Parser.java
@@ -1,12 +1,15 @@
 package com.mdkg.emtcrawler.parser;
 
+import com.mdkg.emtcrawler.model.Category;
 import com.mdkg.emtcrawler.model.Item;
+import com.mdkg.emtcrawler.repository.mock.RepositoryMock;
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 import org.jsoup.select.NodeVisitor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -28,8 +31,10 @@ public  class Parser {
      static String PIVO = "http://www.marketonline.mk/%D0%90%D0%BB%D0%BA%D0%BE%D1%85%D0%BE%D0%BB%D0%BD%D0%B8-%D0%BF%D0%B8%D1%98%D0%B0%D0%BB%D0%BE%D1%86%D0%B8-%D0%9F%D0%B8%D0%B2%D0%BE-64/";
      static String JAJCA ="http://www.marketonline.mk/%D0%88%D0%B0%D1%98%D1%86%D0%B0-%D0%88%D0%B0%D1%98%D1%86%D0%B0-277/";
      static String SVEZH_ZELENCHUK = "http://www.marketonline.mk/%D0%97%D0%B5%D0%BB%D0%B5%D0%BD%D1%87%D1%83%D0%BA-%D0%9E%D1%81%D1%82%D0%B0%D0%BD%D0%B0%D1%82-%D0%B7%D0%B5%D0%BB%D0%B5%D0%BD%D1%87%D1%83%D0%BA-296/";
-    List<String> stringList;
-    Document document;
+
+     List<String> stringList;
+     @Autowired
+     RepositoryMock repository;
     int page = 0;
 
     public Parser() {
@@ -46,23 +51,44 @@ public  class Parser {
     }
 
    // @Scheduled(cron = "0 15 10 15 * ?")
-    public List<Item> crawl(){
-        return null;
-    }
+   public void getAllElements(Document document){
+       StringBuilder sb = new StringBuilder();
+       Category cat =    document.getAllElements()
+               .stream()
+               .filter(e-> e.hasClass("active"))
+               .map(element -> {
+                   Category category  = new Category(element.getElementsByClass("active").text());
+                   return category;
+               }).findFirst().get();
+       List<Item> itemList =  document.getAllElements()
+               .stream()
+               .filter(e-> e.hasClass("thumbnail")).map(element -> {
+                   String priceString = element.getElementsByClass("priceCurrent").text();
+                   Double price = priceString.length()>0 ? Double.parseDouble(priceString.split(" ")[1]) : 0;
+                   Item item = new Item(element.getElementsByClass("image-thumb").attr("title"),price,cat);
+                   return item;
+               }).collect(Collectors.toList());
 
-    public void parseAllSites() throws IOException {
-        Document doc ;
+       repository.addCategory(cat);
+       repository.addItems(itemList);
+       //   .forEach(e-> System.out.println(e.getElementsByClass("image-thumb").attr("title") +" cena = " + e.getElementsByClass("priceCurrent").text()));
 
-        for(String s : stringList){
-            FileWriter writer = new FileWriter ("page"+page+".html");
+
+   }
+
+    public   void buildDatabase(){
+        stringList.stream().forEach(website -> {
+            File file = new File(website);
+            Document doc;
             try {
-                doc = Jsoup.connect(s).get();
-                writer.write(doc.toString());
+                doc = Jsoup.connect(website).get();
+                getAllElements(doc);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            page++;
-        }
+
+
+        });
     }
 
 
