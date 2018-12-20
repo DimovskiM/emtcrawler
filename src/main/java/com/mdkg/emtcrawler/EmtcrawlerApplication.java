68fbf798b5751d2f60947129be84b208f1d34e66
DimovskiM <mihajlo.d96@hotmail.com>
2018-12-20 08:43:16 +0100

    Fixed Category detached entity error. Database building is made on app. startup.

--- src/main/java/com/mdkg/emtcrawler/EmtcrawlerApplication.java
+++ src/main/java/com/mdkg/emtcrawler/EmtcrawlerApplication.java
@@ -1,16 +1,19 @@
 package com.mdkg.emtcrawler;
 
+import com.mdkg.emtcrawler.parser.MockParser;
 import com.mdkg.emtcrawler.parser.Parser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class EmtcrawlerApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(EmtcrawlerApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(EmtcrawlerApplication.class, args);
+		context.getBean(MockParser.class).buildDatabase();
 
 	}
 
--- src/main/java/com/mdkg/emtcrawler/web/TestController.java
+++ src/main/java/com/mdkg/emtcrawler/web/TestController.java
@@ -25,7 +25,6 @@ public class TestController {
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView getItems(){
         ModelAndView modelAndView= new ModelAndView("index.html");
-       itemService.buildDatabase();
 
         List<Item>  itemList = itemService.findAll();
         itemList.stream().forEach(item -> System.out.println(item));
