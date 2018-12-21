2b906d533554cd69ca5c25782b372445a1151a3b
DimovskiM <mihajlo.d96@hotmail.com>
2018-12-21 17:04:48 +0100

    Small bug fixes.

--- src/main/java/com/mdkg/emtcrawler/repository/jpa/ItemRepository.java
+++ src/main/java/com/mdkg/emtcrawler/repository/jpa/ItemRepository.java
@@ -3,13 +3,15 @@ package com.mdkg.emtcrawler.repository.jpa;
 import com.mdkg.emtcrawler.model.Category;
 import com.mdkg.emtcrawler.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item,Integer> {
 
-       Item findById(int ID);
        Item findByName(String name);
        List<Item> findAll();
-       List<Item> findByCategoryName(String category);
+       @Query("select i from Item i where category_name=:category")
+       List<Item> findByCategoryName(@Param("category")String category);
 }
